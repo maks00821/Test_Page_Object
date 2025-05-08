cc1f9b9c971f7533623b7add1052a6de18047a95
maks00821 <nadog2011l15@gmail.com>
2025-05-08 22:37:39 +0300

    Добавление новых файлов в проект и правки основного файла

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,14 @@
 from selenium.webdriver.common.by import By
 import time
+from .pages.main_page import MainPage
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
 
 def test_open_language_page(browser):
-    browser.get(link)
-    button = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
-    assert button is not None, "Кнопка корзины не найдена"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
-    time.sleep(30)
+
+    time.sleep(10)
